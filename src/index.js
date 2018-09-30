c54533c5ab7b87f31aa1c396d95e1ff3efcc0657
Tvorchud <43208345+Tvorchud@users.noreply.github.com>
2018-09-30 23:55:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) { 
+return false; 
+} 
+let massiv = str.split(''); 
+let a,b,c,d = 0; 
+
+for (let i=0;i<massiv.length;i++){ 
+if (massiv[i] = "(") { 
+a++; 
+} 
+if (massiv[i] = ")") { 
+a--; 
+} 
+if (massiv[i] = "[") { 
+b++; 
+} 
+if (massiv[i] = "]") { 
+b--; 
+} 
+if (massiv[i] = "{") { 
+c++; 
+} 
+if (massiv[i] = "}") { 
+c++; 
+} 
+if (massiv[i] = "|") { 
+d++; 
+} 
+} 
+if (a==0 || b==0 || c==0 || d%2==0){ 
+return true; 
+}
 }
