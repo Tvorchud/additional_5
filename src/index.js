c6b845f601e0efdf9fb8d8724add5bd5e271912d
Tvorchud <43208345+Tvorchud@users.noreply.github.com>
2018-10-01 11:48:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  if (str.length % 2 !== 0) { 
-return false; 
-} 
-let massiv = str.split(''); 
-let a,b,c,d = 0; 
-
-for (let i=0;i<massiv.length;i++){ 
-if (massiv[i] = "(") { 
-a++; 
-} 
-if (massiv[i] = ")") { 
-a--; 
-} 
-if (massiv[i] = "[") { 
-b++; 
-} 
-if (massiv[i] = "]") { 
-b--; 
-} 
-if (massiv[i] = "{") { 
-c++; 
-} 
-if (massiv[i] = "}") { 
-c++; 
-} 
-if (massiv[i] = "|") { 
-d++; 
-} 
-} 
-if (a==0 || b==0 || c==0 || d%2==0){ 
-return true; 
-}
+  str = str.split('');
+  if (str.length %2 != 0) return false;
+  else{
+    let err = [];
+      for(let i = 0; i<str.length; i++){
+        for(let j = 0; j< bracketsConfig.length; j++){ 
+          if(str[i] == bracketsConfig[j][1] && err[err.length-1] == bracketsConfig[j][0]) err.pop();
+          else if(str[i] === bracketsConfig[j][0]) err.push(str[i]);
+        }  
+      }
+      if (err.length == 0) return true; 
+      else return false; 
+  }
 }
